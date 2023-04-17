8a588ef1942d8a332b077e6b7d41c4f9ee016429
muneneee <lukekevin39@gmail.com>
2023-04-17 17:43:23 +0300

    Added receipt and invoice models

--- property_management/properties/models.py
+++ property_management/properties/models.py
@@ -19,3 +19,17 @@ class House(models.Model):
     house_rent = models.DecimalField(max_digits=8, decimal_places=2)
     deposit = models.DecimalField(max_digits=8, decimal_places=2)
 
+class Tenant(models.Model):
+    tenant_id = models.CharField(max_length=20)
+    name = models.CharField(max_length=100)
+    contact_info = models.CharField(max_length=100)
+    assigned_unit = models.ForeignKey(House, on_delete=models.CASCADE)
+
+class Invoice(models.Model):
+    tenant = models.ForeignKey(Tenant, on_delete=models.CASCADE)
+    rent = models.DecimalField(max_digits=8, decimal_places=2)
+    garbage_service = models.DecimalField(max_digits=8, decimal_places=2)
+
+class Receipt(models.Model):
+    tenant = models.ForeignKey(Tenant, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=8, decimal_places=2)
