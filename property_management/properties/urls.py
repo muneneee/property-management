f86b5ee6c56f485715fbbdd40ff5e8318266499a
muneneee <lukekevin39@gmail.com>
2023-04-17 19:55:35 +0300

    Added add house functionality

--- property_management/properties/urls.py
+++ property_management/properties/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import add_property, property_list
+from .views import add_property, property_list, add_house
 
 urlpatterns = [
     path('add-property/', add_property, name='add_property'),
     path('property_list/', property_list, name='property_list'),
+    path('add_house/<str:property_id>/', add_house, name='add_house'),
 
 ]
--- property_management/properties/views.py
+++ property_management/properties/views.py
@@ -17,3 +17,18 @@ def add_property(request):
 def property_list(request):
     properties = Property.objects.all()
     return render(request, 'property_list.html', {'properties': properties})
+
+def add_house(request, property_id):
+    property = Property.objects.get(property_id=property_id)
+    if request.method == 'POST':
+        # Get data from form
+        unit_id = request.POST.get('unit_id')
+        house_rent = request.POST.get('house_rent')
+        deposit = request.POST.get('deposit')
+        
+        # Create House object and save to database
+        house = House(property=property, unit_id=unit_id, house_rent=house_rent, deposit=deposit)
+        house.save()
+        
+        return redirect('property_detail', property_id=property_id)
+    return render(request, 'add_house.html', {'property': property})
