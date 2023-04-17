1d11e271830d2c1cccf18b6aec9ba79c7e769524
muneneee <lukekevin39@gmail.com>
2023-04-17 17:42:15 +0300

    Added models

--- property_management/properties/models.py
+++ property_management/properties/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Agent(models.Model):
+    agent_id = models.CharField(max_length=20)
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+
+
+class Property(models.Model):
+    agent = models.ForeignKey(Agent, on_delete=models.CASCADE)
+    property_id = models.CharField(max_length=20)
+    client_details = models.CharField(max_length=100)
+    property_address = models.CharField(max_length=100)
+
+class House(models.Model):
+    property = models.ForeignKey(Property, on_delete=models.CASCADE)
+    unit_id = models.CharField(max_length=20)
+    house_rent = models.DecimalField(max_digits=8, decimal_places=2)
+    deposit = models.DecimalField(max_digits=8, decimal_places=2)
+
