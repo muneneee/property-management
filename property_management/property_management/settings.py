5044f527bafad573d5261acbf74b7430b5a7f6a2
muneneee <lukekevin39@gmail.com>
2023-04-17 18:07:46 +0300

    Added postgresql

--- property_management/property_management/settings.py
+++ property_management/property_management/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'property_management.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'property',
+        'USER': 'kevin',
+        'PASSWORD': '12330122015',
+        'HOST': 'localhost',
     }
 }
 
