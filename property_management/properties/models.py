b582524e1aac65d5aad9202d155f2495f0d43d1d
muneneee <lukekevin39@gmail.com>
2023-04-17 19:46:11 +0300

    Added primary keys to the models

--- property_management/properties/models.py
+++ property_management/properties/models.py
@@ -3,27 +3,30 @@ from django.contrib.auth.models import User
 
 
 class Property(models.Model):
-    property_id = models.CharField(max_length=20)
+    property_id = models.AutoField(primary_key=True)
     client_details = models.CharField(max_length=100)
     property_address = models.CharField(max_length=100)
 
 class House(models.Model):
+    house_id = models.AutoField(primary_key=True)
     property = models.ForeignKey(Property, on_delete=models.CASCADE)
     unit_id = models.CharField(max_length=20)
     house_rent = models.DecimalField(max_digits=8, decimal_places=2)
     deposit = models.DecimalField(max_digits=8, decimal_places=2)
 
 class Tenant(models.Model):
-    tenant_id = models.CharField(max_length=20)
+    tenant_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
     contact_info = models.CharField(max_length=100)
-    assigned_unit = models.ForeignKey(House, on_delete=models.CASCADE)
+    house = models.ForeignKey(House, on_delete=models.CASCADE)
 
 class Invoice(models.Model):
+    invoice_id = models.AutoField(primary_key=True)
     tenant = models.ForeignKey(Tenant, on_delete=models.CASCADE)
     rent = models.DecimalField(max_digits=8, decimal_places=2)
     garbage_service = models.DecimalField(max_digits=8, decimal_places=2)
 
 class Receipt(models.Model):
+    receipt_id = models.AutoField(primary_key=True)
     tenant = models.ForeignKey(Tenant, on_delete=models.CASCADE)
     amount = models.DecimalField(max_digits=8, decimal_places=2)
--- property_management/properties/views.py
+++ property_management/properties/views.py
@@ -4,11 +4,11 @@ from .models import Property, House, Tenant, Invoice, Receipt
 
 def add_property(request):
     if request.method == 'POST':
-        property_id = request.POST['property_id']
+        
         client_details = request.POST['client_details']
         property_address = request.POST['property_address']
         # Create property object and save to the database
-        property = Property(property_id=property_id, client_details=client_details, property_address=property_address)
+        property = Property(client_details=client_details, property_address=property_address)
         property.save()
         return redirect('/view_properties')
     return render(request, 'add_property.html')
